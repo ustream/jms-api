4807072de61f3f6e050890d355755b98db5ad3d4
_nec <nec@shell8.net>
2015-08-17 11:22:45 +0200

    api host from config

--- jms-api.js
+++ jms-api.js
@@ -1,12 +1,25 @@
 var path = require('path');
 var Swaggerize = require('swaggerize-hapi');
 
+
+var apiHosts = {
+	local: '127.0.0.1:1337',
+	beta: 'jms-beta.ustream.tv',
+	live: 'jms.ustream.tv'
+}
+
 module.exports = function (server) {
+
+	var api = require(path.resolve(__dirname,'./config/jms-api.json'));
+
+	if (server.settings.app.api) {
+		api.host = server.settings.app.api.host
+	}
 
 	server.register({
 			register: Swaggerize,
 			options: {
-				api: path.resolve(__dirname,'./config/jms-api.json'),
+				api: api,
 				handlers: path.resolve(__dirname,'./handlers'),
 				cors: {
 					origin: ['*']
