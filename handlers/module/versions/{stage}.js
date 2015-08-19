59b412ef2e24842a1ca2cf5310f9e28fea3132cf
_nec <nec@shell8.net>
2015-08-19 22:08:25 +0200

    refactor version query

--- handlers/module/versions/{stage}.js
+++ handlers/module/versions/{stage}.js
@@ -1,5 +1,26 @@
 'use strict';
 
+var _ = require('lodash');
+var async = require('async');
+
+
+function getVersions (req, item, callback) {
+	var arg =  item.split(':')
+
+	req.server.methods.storage(
+		'getAllVersions',
+		arg[1],
+		arg[2],
+		function (err, result) {
+			callback(err, {
+				source: arg[1],
+				versions: result
+			})
+		}
+	);
+
+}
+
 /**
  * Operations on /module/versions/{stage}
  */
@@ -15,13 +36,17 @@ module.exports = {
 
 		var params = req.params;
 
+		var iterator = _.curry(getVersions)(req);
+
 		req.server.methods.storage(
-			'getSourcesFromStage',
+			'getSourceKeysByStage',
 			params.stage,
 			function (err, result) {
-				reply(result);
+				async.map(result, iterator, function (err, result) {
+					reply(result);
+				})
 			}
-		);
+		)
 	}
 
 };
